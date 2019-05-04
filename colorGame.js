81f75efac177592c5c1598b73d1d13546e263f73
Sara Torrey <sara.moravej@gmail.com>
2019-05-04 17:45:25 -0500

    Add top banner color and fix font Change squares to show 3 for easy and 6 for hard levels

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,48 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors( numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector( "#easyBtn" );
+var hardBtn = document.querySelector( "#hardBtn" );
+
+
+easyBtn.addEventListener( "click", function () {
+    easyBtn.classList.add ("selected");
+    hardBtn.classList.remove ("selected");
+    numSquares = 3;
+    colors = generateRandomColors( numSquares );
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for ( var i = 0; i < squares.length; i++ ) {
+        if ( colors [i] ) {
+            squares [i].style.background = colors[i];
+        } else {
+            squares[i].style.display ="none";
+        }
+    }
+} );
+
+
+hardBtn.addEventListener( "click", function () {
+    hardBtn.classList.add ("selected");
+    easyBtn.classList.remove( "selected" );
+    numSquares = 6;
+    colors = generateRandomColors( numSquares );
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for ( var i = 0; i < squares.length; i++ ) {
+            squares [i].style.background = colors[i];
+            squares[i].style.display ="block";
+        }
+} );
 
 resetButton.addEventListener("click", function () {
     //generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     //pick a new random color from array
     pickedColor = pickColor();
     //change color display to match picked color
@@ -18,7 +51,7 @@ resetButton.addEventListener("click", function () {
     for ( var i = 0; i < squares.length ; i++ ) {
         squares[i].style.background = colors [i];
     }
-    h1.style.background = "#232323";
+    h1.style.background = "steeleblue";
 });
 
 colorDisplay.textContent = pickedColor;
@@ -38,7 +71,7 @@ for (var i = 0; i < squares.length; i++){
             changeColors(clickedColor);
             h1.style.background = clickedColor;
         } else {
-            this.style.background = "#232323";
+            this.style.background = "steeleblue";
             messageDisplay.textContent = "Try again!";
         }
     });
